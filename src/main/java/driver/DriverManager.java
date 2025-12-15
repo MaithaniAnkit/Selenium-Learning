34132da4d000fc5472c9dd4a41abe8da655c590c
MaithaniAnkit <ankitmaithani@gmail.com>
2025-12-15 21:38:00 +0530

    uploading code

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -2,15 +2,22 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class DriverManager {
 
-    WebDriver driver;
-    public void startDriver(){
+    static WebDriver driver;
+    public static void startDriver(){
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--Incognito");
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+    }
 
+    public static void main(String[] args) {
+        DriverManager.startDriver();
     }
 }
